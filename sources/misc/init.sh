8ba23121691742aca377efaad4f08bda5ee9f60f
EwonRael <ewonrael@yahoo.com>
2023-07-07 17:58:40 +0200

    working on black weight

--- sources/misc/init.sh
+++ sources/misc/init.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 export PYTHONV=$(python3 -c 'import sys; v=sys.version_info; print(f"python{v.major}.{v.minor}")')
 export SUBMODULE_INIT=$(git submodule status | grep ^- | wc -l)
-export PYTHONPATH="py/local/lib/$PYTHONV/dist-packages:$(python3 -c "import sys; print(':'.join(sys.path)[1:])")"
+declare _PYTHONPATH _PYTHONPATH2 _OLDPYTHONPATH
+_PYTHONPATH="py/local/lib/$PYTHONV/dist-packages"
+_PYTHONPATH2="${_PYTHONPATH/local}"
+_PYTHONPATH3="${_PYTHONPATH/dist-packages/site-packages}"
+_PYTHONPATH4="${_PYTHONPATH2/dist-packages/site-packages}"
+_PYTHONPATH="${PWD}/${_PYTHONPATH}"
+_PYTHONPATH2="${PWD}/${_PYTHONPATH2}"
+_PYTHONPATH3="${PWD}/${_PYTHONPATH3}"
+_PYTHONPATH4="${PWD}/${_PYTHONPATH4}"
+_OLDPYTHONPATH="$(python3 -c "import sys; print(':'.join(sys.path)[1:])")"
+export PYTHONPATH="$(eval echo "${_PYTHONPATH}:${_PYTHONPATH2}:${_PYTHONPATH3}:${_PYTHONPATH4}:${_OLDPYTHONPATH}")"
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 if test $SUBMODULE_INIT -gt 0; then
   git submodule init
   git submodule update --recursive
